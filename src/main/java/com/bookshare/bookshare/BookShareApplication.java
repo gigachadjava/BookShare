44fa7884644bbe46d768892405ebba5dfd8aeb9b
bohta <bohtarovaha@gmail.com>
2023-12-12 19:17:09 +0300

    Initial commit

---
+++ src/main/java/com/bookshare/bookshare/BookShareApplication.java
@@ -0,0 +1,13 @@
+package com.bookshare.bookshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookShareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookShareApplication.class, args);
+    }
+
+}
